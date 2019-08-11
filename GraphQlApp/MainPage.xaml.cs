8738bebd63f267e9c4ee0d57a427e8e86a90a24e
Ankita Singh <ankitainfinite10@gmail.com>
2019-08-11 19:31:08 -0400

    first commit

--- GraphQlApp/MainPage.xaml.cs
+++ GraphQlApp/MainPage.xaml.cs
@@ -26,31 +26,12 @@ namespace GraphQlApp
         protected override async void OnAppearing()
         {
           
-
-
-            //ResponseLabel.Text = "Fetching data";
-            //var client = new HttpClient();
-            //client.DefaultRequestHeaders.Add("Authorization", "");
-            //client.DefaultRequestHeaders.Add("User-Agent", "");
-            //var stringContent = new StringContent "{/ "query { user(login: kavyaarora) { bio, id, company, name, createdAt } }\"}"};
-
-
-
-            //var httpRes
-
-            //var client = new GraphQLClient("https://api.github.com/graphql");
             var client = new GraphQLClient("https://swapi.apis.guru/");
-           // client.DefaultRequestHeaders.Add("User-Agent", "Graphql Api");
-            //client.DefaultRequestHeaders.Add("Authorization", "bearer 7e434a81dcf48fc753e4aceca78e5c9174747bcc");
-
-            //var stringContent = new StringContent("{\"query\":\"query {user(login: kavyaarora) { bio, id, company, name, createdAt } }\"}");
-
+          
             GraphQLRequest graphQlRequest = new GraphQLRequest()
             {
 
 
-
-                //Query = "query {user(login: kavyaarora) { bio,id,company,name,createdAt }}"
            Query =  "query{ allFilms{ films { id,title,director } }}"
    
 
@@ -73,22 +54,7 @@ namespace GraphQlApp
             film_list.ItemsSource = films;
 
 
-
-            Console.WriteLine("Kavya " +films[0].Title);
-
-            foreach (var film in films)
-            {
-                Console.WriteLine("Title {0}", film.Title);
-            }
-
-
-
-            //posts = JsonConvert.DeserializeObject<List<Models.Film>>(graphQLResponse.Data.allFilms.films.title);
-            ////Assigning the ObservableCollection to MyListView in the XAML of this file
-
-            //Post_List.ItemsSource = posts;
-
-
+        
             base.OnAppearing();
 
         }
--- GraphQlApp/Models/GithubResponse.cs
+++ GraphQlApp/Models/GithubResponse.cs
@@ -1,43 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-
-using System.Globalization;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+﻿using Newtonsoft.Json;
 
 
 namespace  GraphQlApp
 {
-    using System;
-using System.Collections.Generic;
-
-using System.Globalization;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
-public partial class GithubResponse
-{
-    [JsonProperty("data", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
-    public Data Data { get; set; }
-}
-
-public partial class Data
-{
-    [JsonProperty("allFilms", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
-    public AllFilms AllFilms { get; set; }
-}
-
-public partial class AllFilms
-{
-    [JsonProperty("films", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
-    public Film[] Films { get; set; }
-}
-
 public partial class Film
 {
-    [JsonProperty("id", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
-    public string Id { get; set; }
-
+    
     [JsonProperty("title", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
     public string Title { get; set; }
 
